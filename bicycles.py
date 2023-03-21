f41a3f89ce4f768ff7a7ca96b42928a8bd714cbf
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-03-21 19:41:02 +0100

    no message

--- bicycles.py
+++ bicycles.py
@@ -1,2 +1,6 @@
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
-print(bicycles[1].title())
+#multiple objects from list
+print(bicycles[1])
+print(bicycles[3])
+#accesing last object in list
+print(bicycles[-1])
