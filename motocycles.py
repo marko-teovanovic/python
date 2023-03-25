4b27dce3c80c28e30fecd648b443d2263a00d308
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-03-25 13:32:39 +0100

    Removing elements from a list

--- motocycles.py
+++ motocycles.py
@@ -4,7 +4,10 @@ motorcycles.append('honda')
 motorcycles.append('yamaha')
 motorcycles.append('suzuki')
 print(motorcycles)
-#Inserting elements in list
+#Inserting elements in list. [number] - determine where in list we want to add element
 motorcycles.insert(0, 'ducati')
 motorcycles.insert(2, 'harley davidson')
+print(motorcycles)
+#Removing elements from list. [number] - determines witch element we want to remove
+del motorcycles[0]
 print(motorcycles)
