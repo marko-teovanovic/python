f0e9d30b9e030e1d6ec1c6f303144d2259c81628
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-03-25 13:16:45 +0100

    Changing vaule of elements in list

---
+++ motocycles.py
@@ -0,0 +1,6 @@
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+#changing place of element in list, [number] - selecting place for our element in list 
+motorcycles[0] = 'ducati'
+print(motorcycles)
