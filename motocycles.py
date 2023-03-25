29bf062e08641f1e399d3d3689ee7a4995037aad
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-03-25 13:29:53 +0100

    Inserting elements in list

--- motocycles.py
+++ motocycles.py
@@ -1,6 +1,10 @@
 #Creating a list with 'append'
 motorcycles = []
-motorcycles.append('ducati')
+motorcycles.append('honda')
 motorcycles.append('yamaha')
 motorcycles.append('suzuki')
+print(motorcycles)
+#Inserting elements in list
+motorcycles.insert(0, 'ducati')
+motorcycles.insert(2, 'harley davidson')
 print(motorcycles)
