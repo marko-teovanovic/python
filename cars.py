b5d1579e8308667d32ac0fd09997b54c513e603e
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-03-27 16:39:31 +0200

    .sorted() and .reverse() function

--- cars.py
+++ cars.py
@@ -7,3 +7,17 @@ print(cars)
 cars = ['bmw', 'audi', 'opel', 'toyota', 'ford', 'subaru']
 cars.sort(reverse=True)
 print(cars)
+
+#.sorted() function
+cars = ['bmw', 'audi', 'opel', 'toyota', 'ford', 'subaru']
+print("Here is the original list:")
+print(cars)
+print("nHere is the sorted list:")
+print(sorted(cars))
+
+#.reverse() function
+cars = ['bmw', 'audi', 'opel', 'toyota', 'ford', 'subaru']
+print(cars)
+
+cars.reverse()
+print(cars)
