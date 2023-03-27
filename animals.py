9c4c515183d43a444677cde1d6ee17432c1c775d
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-03-27 21:54:48 +0200

    2nd part of 3rd assingment

---
+++ animals.py
@@ -0,0 +1,6 @@
+#3rd assingment 2nd part
+animals = ['cat', 'dog', 'hammster']
+for animal in animals:
+	print(animal.title())
+	print(f"A {animal.title()} would be a great pet!")
+print(f'Any of these animals would a great to have as a pet!')
