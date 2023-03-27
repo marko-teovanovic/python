3a23556affbd6f0cfc03d11e3e0032272b714261
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-03-27 21:50:15 +0200

    First part of 3rd assingment

---
+++ sodas.py
@@ -0,0 +1,7 @@
+#3rd assingment - for statment
+sodas = ['Coca cola', 'pepsi', 'sprite', 'fanta']
+for soda in sodas:
+	print(f"{soda.title()} is a great refreshing drink.\n")
+for soda1 in sodas:
+	print(soda1.title())
+print("I realy like soda's!")
