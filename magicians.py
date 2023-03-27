95c9a98c9d85e64b49f75ac65869bb4cca2d94bc
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-03-27 21:39:50 +0200

    Indent after for

--- magicians.py
+++ magicians.py
@@ -4,3 +4,8 @@ for magician in magicians:
 	print(f"{magician.title()}, that was a great trick.")
 	print(f"I can't wait to see your next trick, {magician.title()}!\n")
 	print("Thank you, everyone. That was a great magic show!")
+
+#What happens if you forget to indent the line afte for statment
+magicians = ['david', 'carolina', 'alice']
+for magician in magicians:
+#print(magician) ---> #IndentationError: expected an indented block after 'for' statement on line 10
