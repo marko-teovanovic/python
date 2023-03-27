171ff0331a4b7cf858c20ee10fc0e0df222dd657
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-03-27 18:04:39 +0200

    Finished 3rd assingment

---
+++ places.py
@@ -0,0 +1,20 @@
+#3rd assingment
+places = ['Miami', 'Amsterdam', 'Tokyo', 'Sydney', 'Dubai']
+print(places)
+print(sorted(places))
+print(f'\nOriginal list:') #Showing that list is in original order
+print(places)
+print('\n')
+print(sorted(places))
+print(f'\nOriginal list') #Showing that list is in original order
+print(places)
+print('\n')
+places.reverse() 
+print(places)
+places.reverse() #Using .reverse() again to return list in original order
+print(places)
+print('\n')
+places.sort()
+print(places)
+places.sort(reverse=True) #Reverse alphabetical order
+print(places)
