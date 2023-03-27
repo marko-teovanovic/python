892959fec064eaad4a2702e28fa5732710678b87
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-03-27 16:35:06 +0200

    Sort and reverse sort

---
+++ cars.py
@@ -0,0 +1,9 @@
+#List sort .sort()
+cars = ['bmw', 'audi', 'opel', 'toyota', 'ford', 'subaru']
+cars.sort()
+print(cars)
+
+#Reverse sort
+cars = ['bmw', 'audi', 'opel', 'toyota', 'ford', 'subaru']
+cars.sort(reverse=True)
+print(cars)
