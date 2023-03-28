3e0c857ea1313237c38ac6a8ba01671fcd53b0d7
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-03-28 21:22:54 +0200

    First numbers in Python

---
+++ first_numbers.py
@@ -0,0 +1,10 @@
+#Off-by-one - this will print numbers from 1 to 4
+for value in range (1, 5):
+	print(value)
+print('\n')
+for value1 in range (1, 6): #Print from 1 to 5, off by
+	print(value1)
+print('\n')
+#Creating a list of numbers with range
+numbers = list(range(1,6))
+print(numbers)
