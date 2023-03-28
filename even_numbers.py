fb0fead8f6daddae33524badb9b25915868ca3e4
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-03-28 21:30:43 +0200

    Even numbers and squares

---
+++ even_numbers.py
@@ -0,0 +1,14 @@
+even_numbers = list(range(2, 11, 2))
+print(even_numbers)
+print('\n')
+#Squer numbers
+squares = []
+for value in range(1, 11):
+	square = value ** 2
+	squares.append(square)
+print(squares)
+#Simplify square equation
+squares1 = []
+for value1 in range(1, 11):
+	squares1.append(value1 **2)
+print(squares1)
