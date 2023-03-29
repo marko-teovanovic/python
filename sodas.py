b17434a720dadbc13eee3a9728b9c07adf0a52ef
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-03-29 17:44:24 +0200

    2nd part of 5th asingment

--- sodas.py
+++ sodas.py
@@ -1,7 +1,15 @@
-#3rd assingment - for statment
 sodas = ['Coca cola', 'pepsi', 'sprite', 'fanta']
 for soda in sodas:
 	print(f"{soda.title()} is a great refreshing drink.\n")
 for soda1 in sodas:
 	print(soda1.title())
 print("I realy like soda's!")
+print('\n') #Continuation of a 5th asingment
+firend_sodas = sodas [:]
+firend_sodas.append('dr. pepper')
+print("My favourite soda's are:")
+for my_soda in sodas:
+	print(my_soda.title())
+print("\nMy friend favourite soda's are:")
+for firend_soda in firend_sodas:
+	print(firend_soda.title())
