59aabbe9723984c59b14a0300dac280f40f1144f
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-03-29 17:32:50 +0200

    Finished 1st part of 5th asingment

--- foods.py
+++ foods.py
@@ -9,6 +9,11 @@ print(friend_foods)
 my_foods.append('gyros')
 friend_foods.append('salad')
 print('\n')
-#We prove that we actualy have 2 different lists
-print(my_foods) 
-print(friend_foods)
+#5th assingment
+print("The first three items in the list are:")
+print(my_foods[:3])
+#print(my_foods) - proof of a list items
+print("Two items from the middle of the list are:")
+print(my_foods[1:4])
+print("The last two items in the list are:")
+print(my_foods[-2:])
