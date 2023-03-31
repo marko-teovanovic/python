78d5fb9b890a8c96b0cc7d998e390e319912171e
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-03-31 20:14:57 +0200

    Finished 6th asingment

---
+++ buffet.py
@@ -0,0 +1,9 @@
+#6th asingment
+print('Restaurant menu:')
+basic_food = ('burgers', 'grain bowls', 'dinner salad', 'rice', 'beans')
+for food in basic_food:
+	print(food.title())
+print('\nChanged rastaurant menu:')
+basic_food = ('cheesburger','grain bowls', 'dinner salad', 'rice', 'beans')
+for food in basic_food:
+	print(food.title())
