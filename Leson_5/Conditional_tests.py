79fead1bfa25ef225b1f08b46ed6ce2db233cca0
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-04-03 22:20:18 +0200

    Finished 7th asingment

--- Leson_5/Conditional_tests.py
+++ Leson_5/Conditional_tests.py
@@ -18,3 +18,17 @@ print(car.lower() == 'subaru') #.lower() converst var to lowercase
 requested_song = 'bohemian rhapsody'
 if requested_song != 'highway to hell':
 	print("\nPlay highway to hell!")
+#Numerical comparison
+age = 17
+if age >= 18:
+	print("\nYou can drive a car!")
+else:
+	print("\nYou are not old enough!")
+football_players = ['Ronaldinho', 'Kaka', 'Vidic', 'Rooney']
+player = 'Van Persie' #If you change value to some from a list you will get a else print.
+if player not in football_players:
+	print(f"\n{player.title()} in not in a list.")
+else:
+	print("Player is already in a list!")
+
+
