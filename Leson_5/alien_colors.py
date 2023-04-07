f473c1d15a126333793539c5e6ea455fc30292a5
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-04-07 21:34:24 +0200

    1st and 2nd part of 8th asingment

---
+++ Leson_5/alien_colors.py
@@ -0,0 +1,13 @@
+#1st part of 8th asingment
+alien_color = ['green', 'yellow', 'red']
+if 'blue' in alien_color:
+	print("Game over!")
+if 'green' in alien_color:
+	print("Player just earned 5 points!")
+print('\n')
+#2nd part of 8th asingment
+alien_color = ['green', 'yellow', 'red']
+if 'blue' in alien_color:
+	print("Player just earned 5 points!")
+else:
+	print("Player just earned 10 point!")
