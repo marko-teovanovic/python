75f814b4fd3a3bb4d3187b2b385ca2437d08c613
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-04-07 21:44:48 +0200

    Finished 8th asingment

--- Leson_5/alien_colors.py
+++ Leson_5/alien_colors.py
@@ -11,3 +11,12 @@ if 'blue' in alien_color:
 	print("Player just earned 5 points!")
 else:
 	print("Player just earned 10 point!")
+print('\n')
+#3rd part of 8th asingment
+alien_color = ['green', 'yellow', 'red']
+if 'green' in alien_color:
+	print("Player just earned 5 points!")
+if 'yellow' in alien_color:
+	print("Player just earned 10 points!")
+if 'red' in alien_color:
+	print("Player just earned 15 points!")
