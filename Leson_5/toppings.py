7a0e0311adef07e6a2858f4058c4c27b9085eab7
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-04-07 21:59:42 +0200

    If inside a for loop

--- Leson_5/toppings.py
+++ Leson_5/toppings.py
@@ -1,5 +1,8 @@
 #If statements with lists, checking for special item in a list
 requested_topping = ['mushrooms', 'extra cheese', 'green peppers']
 for requested_topping in requested_topping:
-	print(f"Adding {requested_topping}")
+	if requested_topping == 'green peppers':
+		print("Sorry, we are out of green peppers right now.")
+	else:
+		print(f"Adding {requested_topping}")
 print("\nFinished making your pizza!")
