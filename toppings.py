bba2c10ac9f849c105418c593aacd6970a6f8a68
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-04-07 21:11:49 +0200

    Multiple statements

--- toppings.py
+++ toppings.py
@@ -1,8 +1,9 @@
-#Inequality
-requested_topping = 'mushrooms'
-if requested_topping != 'anchovies':
-	print("Hold the anchovies!")
-#Checking is value is in a list
-requested_toppings = ['mushrooms', 'onion', 'pineapple']
-'mushrooms' in requested_toppings
-print(requested_toppings)
+#Testing multiple conditions
+requested_topping = ['mushrooms', 'extra cheese']
+if 'mushrooms' in requested_topping:
+	print("Adding mushrooms.")
+if 'peperoni' in requested_topping:
+	print("Adding peperoni.")
+if 'extra cheese' in requested_topping:
+	print("Adding extra cheese.")
+print("\nFinished making your pizza!")
