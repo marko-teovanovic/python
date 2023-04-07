b817722f3fe42d47199cd053ab307548c132e67d
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-04-07 21:52:43 +0200

    Extras for 8th asingment

---
+++ Leson_5/life.py
@@ -0,0 +1,13 @@
+age = 65
+if age <= 2:
+	print("It's a baby!")
+elif age == 2 or age < 4:
+	print("Toddler!")
+elif age == 4 or age < 13:
+	print("Kiddo!")
+elif age == 13 or age < 20:
+	print("Teenager!")
+elif age == 20 or age < 65:
+	print("Adult!")
+elif age >= 65:
+	print("Elder!")
