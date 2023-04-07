0ccb5e128a71a380948a75ce8d6a9cf40a516557
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-04-07 21:01:58 +0200

    no message

--- toppings.py
+++ toppings.py
@@ -2,3 +2,7 @@
 requested_topping = 'mushrooms'
 if requested_topping != 'anchovies':
 	print("Hold the anchovies!")
+#Checking is value is in a list
+requested_toppings = ['mushrooms', 'onion', 'pineapple']
+'mushrooms' in requested_toppings
+print(requested_toppings)
