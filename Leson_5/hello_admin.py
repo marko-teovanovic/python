d6a23e3c4e1ce990d1e7ede6287e8f2972865000
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-04-07 23:47:53 +0200

    Finished 9th asingment

--- Leson_5/hello_admin.py
+++ Leson_5/hello_admin.py
@@ -12,4 +12,15 @@ if usernames:
 			print("We need to find some users!")
 else:
 		print("We need to find some users!")
+print('\n')
+#9th asingment - checking 
+current_users = ['milos', 'dejan', 'djordje', 'stefan', 'aleksandar']
+new_users = ['ivan', 'uros', 'djordje', 'stefan', 'nenad']
+
+current_users_lower = {new_user.lower() for new_user in current_users}
+for new_user in new_users:
+	if new_user in current_users:
+		print(f"Username {new_user.title()} is already taken, please enter new username.")
+	else:
+		print(f"Username {new_user.title()} is available")
 
