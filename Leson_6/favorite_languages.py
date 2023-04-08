5c11c0f785d4a1a869131ee87a800cdfdc9f280a
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-04-08 17:28:02 +0200

    Dictionary - similar objects

---
+++ Leson_6/favorite_languages.py
@@ -0,0 +1,9 @@
+#Similar objects
+favorite_languages = {
+	'jen': 'python',
+	'sarah': 'c',
+	'edward': 'ruby',
+	'phit': 'python',
+}
+language = favorite_languages['sarah'].title()
+print(f"Sarah's favorite language is {language}.")
